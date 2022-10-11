49ca3fd6e041f2ecb64a5fa57fc0b901b67ad7f4
senbar <sendek.bartosz@gmail.com>
2022-10-11 23:13:42 +0200

    redo recipes cells; work on animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -49,21 +49,63 @@ export const slideInAnimation =
           position: 'absolute',
           top: 0,
           left: 0,
-          width: '100%'
+          width: '100%',
+          height: "100vh",
+          "overflow-y": "clip",
+        })
+      ]),
+      query('.waves', [
+        style({
+          position: 'relative',
+          bottom: 0,
+          left: 0,
+          width: '100%',
         })
       ]),
       query(':enter', [
-        style({ right: '-100%' })
+        style({
+          left: '-100%'
+        })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('300ms ease-out', style({ right: '100%' }))
+          animate('400ms ease-out', style({ left: '100%' }))
         ]),
         query(':enter', [
-          animate('300ms ease-out', style({ right: '0%' }))
-        ])
+          animate('400ms ease-out', style({ left: '0%' }))
+        ]),
+        query('.waves', [
+          animate('400ms ease-out', style({ left: '-100%', bottom: "-200%" }))
+        ]),
+
       ]),
       query(':enter', animateChild()),
-    ])]
+    ]),
+    // transition('RecipesPage => HomePage', [
+    //   style({ position: 'relative' }),
+    //   query(':enter, :leave', [
+    //     style({
+    //       position: 'absolute',
+    //       top: 0,
+    //       left: 0,
+    //       width: '100%'
+    //     })
+    //   ]),
+    //   query(':enter', [
+    //     style({ right: '-100%' })
+    //   ]),
+    //   query(':leave', animateChild()),
+    //   group([
+    //     query(':leave', [
+    //       animate('400ms ease-out', style({ right: '100%' }))
+    //     ]),
+    //     query(':enter', [
+    //       animate('400ms ease-out', style({ right: '0%' }))
+    //     ])
+    //   ]),
+    //   query(':enter', animateChild()),
+    // ]
+    // )
+  ]
   );
--- src/app/modules/recipes/model/recipe.dto.ts
+++ src/app/modules/recipes/model/recipe.dto.ts
@@ -2,6 +2,7 @@ export class RecipeDTO {
   public Method: String = "";
   public Coffee: String = "Coffee";
   public Name: String = "Coffee recipe";
+  public Duration: number = 23;
   public Author: String = "Anomylous";
   public Temperature?: number;
   public TimeInSeconds: number = 0;
